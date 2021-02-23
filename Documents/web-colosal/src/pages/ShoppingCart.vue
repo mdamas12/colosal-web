028e5a8401cac87dc3e93a8e9f0835e80cba8250
Eduardo Hernandez <eduardo.hernandez.puga@gmail.com>
2021-02-23 16:35:53 -0400

    maqueta de carrito de compras

---
+++ Documents/web-colosal/src/pages/ShoppingCart.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page>
+    <example-component
+      title="Example component"
+    ></example-component>
+    <shopping-cart-component></shopping-cart-component>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import ShoppingCartComponent from '../components/cart/ShoppingCartComponent.vue'
+
+export default defineComponent({
+  name: 'ShoppingCart',
+  components: { ShoppingCartComponent }
+})
+</script>
